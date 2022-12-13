196afbb671077b78ea9d9509d043f43febe0f8b6
0xjimmy <contact@decentralised.tech>
2022-12-14 12:15:36 +1300

    Add dev redirects to hit http endpoints on https frontend

--- src/lib/state/state.js
+++ src/lib/state/state.js
@@ -5,103 +5,97 @@
 import { writable } from 'svelte/store';
 
 export const connectedAddress = writable();
-export const connectedUsername = writable()
+export const connectedUsername = writable();
 
-export const isHostAddress = writable()
-export const isTwoAddress = writable()
-
+export const isHostAddress = writable();
+export const isTwoAddress = writable();
 
 export const isProfile = writable();
 export const fetchedProfile = writable();
 export const profileBadges = writable([]);
 
-export const userID = writable(); // Allows users to play with their address or username. If no username, assigns address. 
+export const userID = writable(); // Allows users to play with their address or username. If no username, assigns address.
 export const connectedChain = writable(false);
 
-export const getBalance = writable() 
+export const getBalance = writable();
 
-export const playerNotifications = writable()
+export const playerNotifications = writable();
 
-export const createPrompt = writable(false)
-export const tipPrompt = writable(false)
+export const createPrompt = writable(false);
+export const tipPrompt = writable(false);
 
-export const notiPrompt = writable(false)
-export const authPrompt = writable(false)
+export const notiPrompt = writable(false);
+export const authPrompt = writable(false);
 
-
-
-export const selectedOption = writable('Chess')
-export const inGame = writable(false)
-export const medalAlert = writable(false)
-export const creatingGame = writable()
+export const selectedOption = writable('Chess');
+export const inGame = writable(false);
+export const medalAlert = writable(false);
+export const creatingGame = writable();
 
 // POSTGRES ENDPOINTS
-export const url0 = 'http://170.187.182.220:5001/address'
-export const url1 = 'http://170.187.182.220:5001/username'
-export const url2 = 'http://170.187.182.220:5001/getaddr?username='
-export const getXp = 'http://170.187.182.220:5001/getxp?user=' // May not need this.
-export const notificationsUrl = 'http://170.187.182.220:5001/getnotifications'
-export const getProfileURL = 'http://170.187.182.220:5001/getprofile?user='
+export const url0 = '/api/address';
+export const url1 = '/api/username';
+export const url2 = '/api/getaddr?username=';
+export const getXp = '/api/getxp?user='; // May not need this.
+export const notificationsUrl = '/api/getnotifications';
+export const getProfileURL = '/api/getprofile?user=';
 // REDIS ENDPOINTS
 
+export const makeBallRoom = 'http://172.105.106.183:5020/makeballroom?user=';
+export const getBallRoomsUrl = 'http://172.105.106.183:5020/getballrooms';
 
-export const makeBallRoom = 'http://172.105.106.183:5020/makeballroom?user='
-export const getBallRoomsUrl = 'http://172.105.106.183:5020/getballrooms'
+export const urlRooms = 'http://172.105.106.183:5020/rooms';
+export const urlEndedRooms = 'http://172.105.106.183:5020/endedrooms';
 
-export const urlRooms = 'http://172.105.106.183:5020/rooms'
-export const urlEndedRooms = 'http://172.105.106.183:5020/endedrooms'
-
-export const tipSocket = 'http://172.105.106.183:4903/'
+export const tipSocket = 'http://172.105.106.183:4903/';
 
 // chess contract, aswell as endpoint that Listens for contract events via trongrid.io API, such as the index which is a nessescary parameter to invoke the relevant struct.
 export const tipContract = 'TBPL4jVJMwnMLUjmX9GDTgXcC8y3T5zVgD';
-export const chessContract = 'TMGBGionnPs1TFRHxNrZRiGneZaDi6zkBh'
-export const _chessContract  = 'TQyY41mqbHVWWHWt5Zq1pPL5rYd7HgM2kE'
+export const chessContract = 'TMGBGionnPs1TFRHxNrZRiGneZaDi6zkBh';
+export const _chessContract = 'TQyY41mqbHVWWHWt5Zq1pPL5rYd7HgM2kE';
 
+export const chessEventListener = `https://api.shasta.trongrid.io/v1/contracts/${chessContract}/events`;
 
-export const chessEventListener = `https://api.shasta.trongrid.io/v1/contracts/${chessContract}/events` 
+export const eventAPI = 'http://172.105.106.183:5020/api';
 
-export const eventAPI = 'http://172.105.106.183:5020/api'
- 
 // Chess socket.io endpoint
 export const chessWs = 'http://172.105.106.183:3001';
 
-export const currentState = writable('') // Saves current FEN state of chess game.
-export const wagerTx = writable()
-export const theRoom = writable()
-let prompted = false
-export async function createGameForm() { // Create game prompt state sharing between components
-    prompted = !prompted
-    createPrompt.set(prompted)
+export const currentState = writable(''); // Saves current FEN state of chess game.
+export const wagerTx = writable();
+export const theRoom = writable();
+let prompted = false;
+export async function createGameForm() {
+	// Create game prompt state sharing between components
+	prompted = !prompted;
+	createPrompt.set(prompted);
 }
 
-
-let isTipExpanded = false
+let isTipExpanded = false;
 export async function tipPlayerForm() {
-    isTipExpanded = !isTipExpanded
-    console.log(isTipExpanded)
-    tipPrompt.set(isTipExpanded)
+	isTipExpanded = !isTipExpanded;
+	console.log(isTipExpanded);
+	tipPrompt.set(isTipExpanded);
 }
 
-
-let authExpanded = false
+let authExpanded = false;
 export function myProfile() {
-    authExpanded = !authExpanded
-    authPrompt.set(authExpanded)
+	authExpanded = !authExpanded;
+	authPrompt.set(authExpanded);
 }
 
-let notificationExpanded = false
+let notificationExpanded = false;
 export function notificationPrompt() {
-    notificationExpanded = !notificationExpanded
-    notiPrompt.set(notificationExpanded)
+	notificationExpanded = !notificationExpanded;
+	notiPrompt.set(notificationExpanded);
 }
 
 export async function postRequest(url, body) {
-    const res = await fetch(url, {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: body,
-    })
-    if (!res.ok) throw new Error(`${res.status}: ${res.statusText}`)
-    return await res.json()
-} 
+	const res = await fetch(url, {
+		method: 'post',
+		headers: { 'Content-Type': 'application/json' },
+		body: body
+	});
+	if (!res.ok) throw new Error(`${res.status}: ${res.statusText}`);
+	return await res.json();
+}
